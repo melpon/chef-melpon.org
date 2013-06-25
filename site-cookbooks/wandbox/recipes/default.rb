4e14dbdc8c1cc0f7544eab4bfadb1303ba7bd6d2
melpon <melt@e-ml.jp>
2013-06-25 22:16:35 +0900

    Add recipes

--- site-cookbooks/wandbox/recipes/default.rb
+++ site-cookbooks/wandbox/recipes/default.rb
@@ -1,13 +1,28 @@
 #
-# Cookbook Name:: haskell
+# Cookbook Name:: wandbox
 # Recipe:: default
 #
 # Copyright 2013, YOUR_COMPANY_NAME
 #
 # All rights reserved - Do Not Redistribute
 #
-include_recipe "haskell"
+user node.travis_build_environment.user do
+  action :create
+  home node.travis_build_environment.home
+  supports :manage_home => true
+  shell "/bin/bash"
+end
+
 include_recipe "boost"
+include_recipe "haskell"
+
+ruby_block "insert_line" do
+  block do
+    file = Chef::Util::FileEdit.new(node.travis_build_environment.home + "/.bashrc")
+    file.insert_line_if_no_match("/.cabal\\/bin/", "export PATH=$HOME/.cabal/bin:$PATH")
+    file.write_file
+  end
+end
 
 package "git" do
   action :install
@@ -36,6 +51,19 @@ git node.travis_build_environment.home + "/wandbox" do
   group node.travis_build_environment.group
 end
 
+script "make cattleshed" do
+  action :run
+  cwd node.travis_build_environment.home + "/wandbox/cattleshed"
+  user node.travis_build_environment.user
+  group node.travis_build_environment.group
+
+  interpreter "bash"
+
+  code <<-SH
+  make
+  SH
+end
+
 script "install kennel" do
   action :run
   cwd node.travis_build_environment.home + "/wandbox/kennel"
@@ -46,5 +74,18 @@ script "install kennel" do
 
   code <<-SH
   cabal install
+  SH
+end
+
+script "run" do
+  action :run
+  user node.travis_build_environment.user
+  group node.travis_build_environment.group
+
+  code <<-SH
+  cd #{node.travis_build_environment.home + "/wandbox/cattleshed"}
+  nohup ./server.exe &
+  cd #{node.travis_build_environment.home + "/wandbox/kennel"}
+  nohup ./dist/bin/kennel Production &
   SH
 end
