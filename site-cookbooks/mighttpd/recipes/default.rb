ad347d5d22ca36846399fbaffdecd4de2e73f72d
melpon <melt@e-ml.jp>
2013-07-07 15:17:14 +0900

    now working.

--- site-cookbooks/mighttpd/recipes/default.rb
+++ site-cookbooks/mighttpd/recipes/default.rb
@@ -26,11 +26,15 @@ bash "install mighttpd" do
   not_if "su - mighttpd -c 'test -e .cabal/bin/mighty'"
 end
 
-cookbook_file "/etc/init/mighttpd.conf" do
-  action :create
+cookbook_file "/home/mighttpd/mighttpd.server.conf" do
+  user 'mighttpd'
+  group 'mighttpd'
+  mode '0644'
+end
 
-  user 'root'
-  group 'root'
+cookbook_file "/home/mighttpd/mighttpd.server.route" do
+  user 'mighttpd'
+  group 'mighttpd'
   mode '0644'
 end
 
@@ -40,19 +44,11 @@ bash "run mighttpd" do
   code "start mighttpd"
 end
 
-cookbook_file "/home/mighttpd/mighttpd.server.conf" do
-  user 'mighttpd'
-  group 'mighttpd'
+cookbook_file "/etc/init/mighttpd.conf" do
+  user 'root'
+  group 'root'
   mode '0644'
 
   notifies :run, "bash[run mighttpd]", :immediately
-end
-
-cookbook_file "/home/mighttpd/mighttpd.server.route" do
-  action :create
-
-  user 'mighttpd'
-  group 'mighttpd'
-  mode '0644'
 end
 
