1506ceaa42e675e65f61feecf4535b3913231b34
melpon <melt@e-ml.jp>
2013-07-26 12:44:00 +0900

    add perl-5.19.2

--- site-cookbooks/perl/recipes/default.rb
+++ site-cookbooks/perl/recipes/default.rb
@@ -7,7 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-def install_perl(source, file, build_dir, prefix)
+def install_perl(source, file, build_dir, prefix, configure_flags)
   remote_file "#{Chef::Config[:file_cache_path]}/#{file}" do
     source source + file
     mode '0644'
@@ -20,9 +20,10 @@ def install_perl(source, file, build_dir, prefix)
     cwd Chef::Config[:file_cache_path]
 
     code <<-EOH
-    tar -xf perl-5.18.0.tar.gz
+    set -e
+    tar -xf #{file}
     cd #{build_dir}
-    ./Configure -des -Dprefix=#{prefix}
+    ./Configure -des -Dprefix=#{prefix} #{configure_flags}
     make
     make install
 
@@ -33,4 +34,5 @@ def install_perl(source, file, build_dir, prefix)
   end
 end
 
-install_perl('http://www.cpan.org/src/5.0/', 'perl-5.18.0.tar.gz', 'perl-5.18.0', '/usr/local/perl-5.18.0')
+install_perl('http://www.cpan.org/src/5.0/', 'perl-5.18.0.tar.gz', 'perl-5.18.0', '/usr/local/perl-5.18.0', '')
+install_perl('http://www.cpan.org/src/5.0/', 'perl-5.19.2.tar.gz', 'perl-5.19.2', '/usr/local/perl-5.19.2', '-Dusedevel')
