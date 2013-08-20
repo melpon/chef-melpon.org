5f65d01bddce7e5021c13f884bd03d464ca722c7
melpon <melt@e-ml.jp>
2013-08-20 23:05:15 +0900

    add boost 1.54.0

--- site-cookbooks/boost/recipes/default.rb
+++ site-cookbooks/boost/recipes/default.rb
@@ -1,34 +1,51 @@
 include_recipe "build-essential"
 
-remote_file "#{Chef::Config[:file_cache_path]}/#{node.boost.file}" do
-  source node.boost.source + node.boost.file
-  mode "0644"
-  action :create_if_missing
-  not_if "test -d #{node.boost.prefix}"
-end
+def install_boost(source, file, build_dir, prefix, bjam_flags, update_profile)
+  remote_file "#{Chef::Config[:file_cache_path]}/#{file}" do
+    source source + file
+    mode "0644"
+    action :create_if_missing
+    not_if "test -d #{prefix}/include/boost"
+  end
 
-bash "install-boost" do
-  user "root"
-  cwd Chef::Config[:file_cache_path]
-  code <<-EOH
-  set -e
-  tar xf #{node.boost.file}
-  cd #{node.boost.build_dir}
-  ./bootstrap.sh
-  ./bjam install --prefix=#{node.boost.prefix} #{node.boost.bjam_flags} > /dev/null
-  cd ../
-  rm -r #{node.boost.build_dir}
-  EOH
-  not_if "test -d #{node.boost.prefix}"
-end
+  bash "install-boost" do
+    user "root"
+    cwd Chef::Config[:file_cache_path]
+    code <<-EOH
+    set -e
+    tar xf #{file}
+    cd #{build_dir}
+    ./bootstrap.sh
+    ./bjam install --prefix=#{prefix} #{bjam_flags} > /dev/null
+    cd ../
+    rm -r #{build_dir}
+    EOH
+    not_if "test -d #{prefix}/include/boost"
+  end
 
-if node.boost.update_profile then
-  file "/etc/profile.d/boost.sh" do
-    mode "0755"
-    content <<-SH
-      export LIBRARY_PATH=#{node.boost.prefix}/lib:$LIBRARY_PATH
-      export LD_LIBRARY_PATH=#{node.boost.prefix}/lib:$LD_LIBRARY_PATH
-      export CPLUS_INCLUDE_PATH=#{node.boost.prefix}/include:$CPLUS_INCLUDE_PATH
-    SH
+  if update_profile then
+    file "/etc/profile.d/boost.sh" do
+      mode "0755"
+      content <<-SH
+        export LIBRARY_PATH=#{prefix}/lib:$LIBRARY_PATH
+        export LD_LIBRARY_PATH=#{prefix}/lib:$LD_LIBRARY_PATH
+        export CPLUS_INCLUDE_PATH=#{prefix}/include:$CPLUS_INCLUDE_PATH
+      SH
+    end
   end
 end
+
+install_boost(
+  'http://sourceforge.net/projects/boost/files/boost/1.47.0/',
+  'boost_1_47_0.tar.gz',
+  'boost_1_47_0',
+  '/usr/local/boost-1.47.0',
+  '--with-system --with-program_options',
+  false)
+install_boost(
+  'http://sourceforge.net/projects/boost/files/boost/1.54.0/',
+  'boost_1_54_0.tar.gz',
+  'boost_1_54_0',
+  '/usr/local/boost-1.54.0',
+  '--with-system --with-program_options',
+  false)
