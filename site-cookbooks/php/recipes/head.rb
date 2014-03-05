f70fdaa94b90815d7586fc143729e143eeb0793b
melpon <melt@e-ml.jp>
2014-03-06 00:06:29 +0900

    add php HEAD

---
+++ site-cookbooks/php/recipes/head.rb
@@ -0,0 +1,44 @@
+include_recipe 'heads'
+
+build_user = 'heads'
+build_home = '/home/' + build_user
+build_dir = build_home + '/php-src'
+prefix = '/usr/local/php-head'
+build_sh = build_home + '/build/php.sh'
+
+bash 'git clone php' do
+  action :run
+  user build_user
+  cwd build_home
+  code "git clone https://github.com/php/php-src.git"
+  not_if "test -d #{build_home + '/php-src'}"
+end
+
+file build_sh do
+  mode '0755'
+  user 'root'
+  group 'root'
+  content <<-SH
+  set -ex
+  su - #{build_user} -c '
+    cd #{build_dir}
+    git clean -xdqf
+    git checkout master
+    git pull origin master
+
+    ./buildconf
+    ./configure --prefix=#{prefix}
+    nice make
+  '
+  cd #{build_dir}
+  nice make install
+  SH
+end
+
+# test building
+bash 'test building php-head' do
+  action :run
+  user 'root'
+  code build_sh
+  not_if "test -e #{prefix + '/bin/php'}"
+end
