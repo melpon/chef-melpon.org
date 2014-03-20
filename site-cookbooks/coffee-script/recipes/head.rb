2aef282bdd8efe8e3d3b6b85e6963a233dac6fd6
melpon <melt@e-ml.jp>
2014-03-21 02:06:23 +0900

    set building user heads for coffee-script HEAD.

--- site-cookbooks/coffee-script/recipes/head.rb
+++ site-cookbooks/coffee-script/recipes/head.rb
@@ -1,4 +1,11 @@
+include_recipe 'heads'
+
+build_user = 'heads'
+build_home = '/home/' + build_user
+build_dir = build_home + '/coffee-script'
 prefix = '/usr/local/coffee-script-head'
+build_sh = build_home + '/build/coffee-script.sh'
+
 npm = '/usr/local/node-0.10.24/bin/npm'
 
 bash 'install coffee-script HEAD' do
@@ -12,10 +19,22 @@ bash 'install coffee-script HEAD' do
   not_if "test -e #{prefix}/bin/coffee"
 end
 
-cron 'update_coffee-script_head' do
-  action :create
-  minute '0'
-  hour '9'
-  command "cd #{prefix}; git pull; rm -r node_modules; #{npm} install"
-  mailto node['email']
+file build_sh do
+  mode '0755'
+  user 'root'
+  group 'root'
+  content <<-SH
+    cd #{prefix}
+    git pull
+    rm -r node_modules
+    #{npm} install
+  SH
+end
+
+# test building
+bash 'test building coffee-script-head' do
+  action :run
+  user 'root'
+  code build_sh
+  not_if "test -d #{prefix + '/node_modules'}"
 end
