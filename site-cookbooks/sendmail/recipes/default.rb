f026aa956f7e57a61b7c2e5a1025f0c8ff440430
melpon <melt@e-ml.jp>
2014-03-29 00:17:28 +0900

    sendmail settings.

--- site-cookbooks/sendmail/recipes/default.rb
+++ site-cookbooks/sendmail/recipes/default.rb
@@ -7,3 +7,33 @@
 # All rights reserved - Do Not Redistribute
 #
 package 'sendmail-bin'
+
+domain = 'melpon.org'
+
+bash 'set mailaddress' do
+  action :run
+  user 'root'
+  code <<-SH
+    set -ex
+    echo "Dm#{domain}" >> /etc/mail/sendmail.mc
+    echo "Cw#{domain}" >> /etc/mail/sendmail.mc
+    echo "define(\\`confDOMAIN_NAME', \\`#{domain}')dnl" >> /etc/mail/sendmail.mc
+    echo "MASQUERADE_AS(\\`#{domain}')dnl" >> /etc/mail/sendmail.mc
+    m4 /etc/mail/sendmail.mc > /etc/mail/sendmail.cf
+    service sendmail restart
+  SH
+  not_if "cat /etc/mail/sendmail.mc | grep 'confDOMAIN_NAME'"
+end
+
+bash 'set hostname' do
+  action :run
+  user 'root'
+  code <<-SH
+    set -ex
+    sed "s/ubuntu.localdomain/melpon.org/" /etc/hosts > /tmp/hosts
+    mv /tmp/hosts /etc/hosts
+    chmod 644 /etc/hosts
+  SH
+  only_if "cat /etc/hosts | grep 'ubuntu.localdomain'"
+end
+
