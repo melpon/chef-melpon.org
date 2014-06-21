40b5b40e73a95370906288839cb60ce0d960b596
melpon <melt@e-ml.jp>
2014-06-21 22:37:30 +0900

    fix Rust compilation error.

--- site-cookbooks/rust/recipes/head.rb
+++ site-cookbooks/rust/recipes/head.rb
@@ -7,7 +7,7 @@ prefix = '/usr/local/rust-head'
 build_sh = build_home + '/build/rust.sh'
 
 git_repo = 'https://github.com/mozilla/rust.git'
-gcc_root = '/usr/local/gcc-4.8.2'
+with_gcc = '/usr/local/gcc-4.8.2'
 
 bash 'git clone rust' do
   action :run
@@ -30,19 +30,47 @@ file build_sh do
   cd #{build_dir}
   git clean -xdqf
   chown heads:heads -R ./
+
+  export PATH=#{with_gcc}/bin:$PATH
+  export LD_LIBRARY_PATH=#{with_gcc}/lib64:$LD_LIBRARY_PATH
+
   su - #{build_user} -c '
     set -ex
     cd #{build_dir}
     git reset --hard
-    git pull
     git clean -xdqf
+    git pull
+    git submodule update -i
 
-    export PATH=#{gcc_root}/bin:$PATH
-    ./configure --prefix=#{prefix} --disable-docs --disable-llvm-assertions --disable-debug
-    nice make -j3
+    cd #{build_dir}/src/libuv
+    export CC="gcc -fPIC"
+    ./autogen.sh
+    ./configure --prefix=#{build_dir}/libuv
+    nice make
+    nice make check
+    nice make install
+
+    cd #{build_dir}
+    export PATH="#{with_gcc}/bin:$PATH"
+    export CC="#{with_gcc}/bin/gcc"
+    export CXX="#{with_gcc}/bin/g++"
+    export LDFLAGS="-Wl,-rpath,#{with_gcc}/lib64"
+    export RUSTFLAGS="-C linker=#{with_gcc}/bin/g++ -C link-args=-Wl,-rpath,#{with_gcc}/lib64"
+    ./configure --prefix=#{prefix} --sysconfdir=#{prefix}/etc --disable-docs --disable-llvm-assertions --disable-debug --enable-llvm-static-stdcpp --libuv-root=#{build_dir}/libuv/lib
+    nice make RUSTFLAGS="$RUSTFLAGS" -j3 -k
+    nice make RUSTFLAGS="$RUSTFLAGS" -j3 -k
+    nice make RUSTFLAGS="$RUSTFLAGS"
   '
   cd #{build_dir}
   nice make install
+
+  echo '#!/bin/sh
+export PATH=#{with_gcc}/bin:$PATH
+export LD_LIBRARY_PATH=#{with_gcc}/lib64:$LD_LIBRARY_PATH
+#{prefix}/bin/rustc "$@"
+  ' > #{prefix}/bin/run-rustc.sh
+  chmod +x #{prefix}/bin/run-rustc.sh
+
   SH
 end
 
